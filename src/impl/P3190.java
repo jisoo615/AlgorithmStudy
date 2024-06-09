dc8d0ad584dc90b641fba5587bcc925a30dacc29
jisoo <ske050588@gmail.com>
2024-06-09 22:12:12 +0900

    백준 3190 뱀 복잡구현

--- src/impl/P3190.java
+++ src/impl/P3190.java
@@ -18,48 +18,57 @@ public class P3190 {
         int K = Integer.parseInt(br.readLine());
         StringTokenizer st;
         int x, y;
-        Deque<Info> dq = new LinkedList<>();
+        Deque<Info> infoDq = new LinkedList<>();
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
+            x = Integer.parseInt(st.nextToken()) - 1;
+            y = Integer.parseInt(st.nextToken()) - 1;
             map[x][y] = -1;
         }
         int L = Integer.parseInt(br.readLine());
         for (int i = 0; i < L; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            dq.add(new Info(Integer.parseInt(st.nextToken()), st.nextToken()));
+            infoDq.add(new Info(Integer.parseInt(st.nextToken()), st.nextToken()));
         }
 
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
+        int[] dx = {-1, 0, 1, 0};// 상우하좌
+        int[] dy = {0, 1, 0, -1};
+        int d = 1;// 우
         // 뱀이 0,0에서 시작
-        Queue<Point> q = new LinkedList<>();
         Deque<Point> snake = new LinkedList<>();
-        q.add(new Point(0, 0));
-        int second = 0;// 벽이나 자신 몸에 부딪히면 끝
         map[0][0] = 1;
         snake.add(new Point(0, 0));
-        while(!q.isEmpty()){
-            Point node = q.poll();
-            for (int i = 0; i < 4; i++) {//TODO: 방향이 정해지면 일관된 방향으로 가는 걸로 바꾸기
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if(nx<0 || nx>N || ny<0 || ny>N) continue;// 벽에 부딪힘
-                if(map[nx][ny]==1) continue;// 몸에 부딪힘
+        int second = 0;
 
-                if(map[nx][ny]==-1){// 사과있음
-                    map[nx][ny] = map[node.x][node.y] + 1;// 초를 저장
-                }else{// 사과없음
-                    snake.pollFirst();
-                    map[nx][ny] = map[node.x][node.y] + 1;// 초를 저장
-                }
+        while(true){
+            second++;
+            Point node = snake.peekLast();
+
+            int nx = node.x + dx[d];
+            int ny = node.y + dy[d];
+            if(nx<0 || nx>=N || ny<0 || ny>=N) break;// 벽에 부딪힘
+            if(map[nx][ny]>=1) break;// 몸에 부딪힘
+            if(map[nx][ny]==-1){// 사과있음
+                map[nx][ny] = 1;
                 snake.add(new Point(nx, ny));
+            }else{// 사과없음
+                Point tail = snake.pollFirst();
+                map[nx][ny] = 1;
+                map[tail.x][tail.y] = 0;
+                snake.add(new Point(nx, ny));
+            }
 
+            Info info = infoDq.peekFirst();
+            if(!infoDq.isEmpty() && second == info.t){
+                infoDq.removeFirst();
+                if(info.dir.equals("D")){// 오른쪽
+                    d = d+1 > 3 ? 0 : d+1;
+                }else{// L 왼쪽
+                    d = d-1 <0 ? 3 : d-1;
+                }
             }
         }
-        
-
+        System.out.println(second);
     }
     static class Info{
         int t;
